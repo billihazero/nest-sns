f1bf7bb6c02eaa3f7ed90f389c5ab28a15bfefe3
billihazero <billihazero@gmail.com>
2025-09-05 11:45:35 +0900

    feat: Post CRUD 기능 추가
    - nest g resource 명령어를 통해 post 리소스 자동 생성.
    - post CRUD 구현

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,138 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+
+interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 100000,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '노래 연습하고 있는 해린',
+    likeCount: 100000,
+    commentCount: 999999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '공연하고 있는 로제',
+    likeCount: 100000,
+    commentCount: 999999,
+  },
+];
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+  //1) GET /posts
+  // 모든 post를 다 가져온다.
+  @Get('/')
+  getPosts() {
+    return posts;
+  }
+  //2) GET /posts/:id
+  //id 에 해당하는 post를 가져온다.
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === +id); //parameter에서 받아온 string type인 id에 +를 붙이면 number type으로 비교된다.
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  //3) POST /posts
+  //post 생성한다.
+  @Post('/')
+  postPosts(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts = [...posts, post];
+    return post;
+  }
+
+  //4) PUT /posts/:id
+  //id 에 해당하는 post를 수정한다.
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+    if (author) {
+      post.author = author;
+    }
+    if (title) {
+      post.title = title;
+    }
+    if (content) {
+      post.content = content;
+    }
+    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
+    return post;
+  }
+
+  //5) DELETE /posts/:id
+  //id에 해당하는 post를 삭제한다.
+  @Delete(':id')
+  deletePost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id == +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    posts = posts.filter((post) => post.id !== +id); //해당 id인 post만 빼고 다시 posts에 저장
+
+    return id;
+  }
+}
